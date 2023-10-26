a6f9662a1135d2273b52cb4e257570e99107b93c
ach-ber <77842805+ach-ber@users.noreply.github.com>
2023-10-26 16:50:06 +0200

    api gateway + eureka

---
+++ eureka/src/test/java/com/iwa/eureka/EurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iwa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+ */
