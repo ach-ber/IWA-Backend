ceb9b8f87e3c3d0f591e2c09a2a9cb01260a887b
Océan Sicard--Baux <ocean.sicard-baux@etu.umontpellier.fr>
2023-11-13 11:54:58 +0100

    job service: remove unused files

--- service-job/src/main/java/com/iwa/jobservice/recipe/RecipeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.iwa.jobservice.recipe;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
