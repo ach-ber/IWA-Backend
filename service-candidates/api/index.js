33e4c7cbe487a90e9fb3d024e3ad8817e57a1072
Océan Sicard--Baux <ocean.sicard-baux@etu.umontpellier.fr>
2023-11-14 10:27:03 +0100

    rename candidate service + fix docker image (and use alpine instead of debian)

--- service-candidates/api/index.js
+++ service-candidates/api/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const HOST = "localhost"
+const HOST = "0.0.0.0"
 
 app.get("/candidates", (req, res) => {
     res.sendFile(__dirname + "/fake_candidates.json")
 })
 
 app.listen(PORT, HOST, () => {
-    console.log(`Candidates API available on http://${HOST}:${PORT}/candidates`)
+    console.log(`Candidates API available on http://localhost:${PORT}/candidates`)
 })
